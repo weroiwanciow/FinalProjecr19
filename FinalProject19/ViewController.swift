431091f9df827215118dc8e1726eec7179779591
Mama 3 <Mama@Anettas-iMac.local>
2019-08-01 22:53:42 -0400

    first commit

---
+++ FinalProject19/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  FinalProject19
+//
+//  Created by Apple on 7/31/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBAction func optionA(_ sender: Any) {
+        
+    }
+    
+    @IBAction func optionB(_ sender: Any) {
+    }
+    
+    @IBAction func optionC(_ sender: Any) {
+    }
+    
+    @IBAction func optionD(_ sender: Any) {
+    }
+    
+
+        
+        
+        
+    }
+    
+    
+    
+    
+    /*  override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        timePlaylistButton.addTarget(self, action: "didTapTime", forControlEvents: .TouchUpInside)
+        }
+        
+        @IBAction func didTapTime(sender: AnyObject) {
+            UIApplication.sharedApplication().openURL(NSURL(string: "https://open.spotify.com/playlist/39zT0jaKtTgi22rpc5PRWK?si=x8NvskWrSWSaQGJhhjUtmQ")!)
+        }
+*/
+
+
+
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# finalprojectclone
